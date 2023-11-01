3ac7b165fb7d493496dcb36ad9e9d0de33b94692
John Ault <john.ault.dev@gmail.com>
2023-10-31 20:59:14 -0400

    Updated style

--- frontend/src/components/PageLogin.jsx
+++ frontend/src/components/PageLogin.jsx
@@ -4,20 +4,28 @@ import Plaintext from "./SectionPlainText";
 export default function PageLogin() {
   return (
     <>
-      <PageTitle title={"Login"} />
-      <Plaintext>
-        <p className="lead">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam optio
-          deserunt possimus autem fuga? Soluta maxime corporis quasi dolor at,
-          vero, laboriosam vel dolorem eaque enim molestias omnis non inventore!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          quibusdam, dolores sit possimus natus beatae debitis ratione officia
-          obcaecati exercitationem, dolore blanditiis ipsam cupiditate eius,
-          repellat nisi voluptatum quam aliquid.
-        </p>
-      </Plaintext>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+          <PageTitle title={"Login"} />
+              <Plaintext>
+                <p className="lead">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam optio
+                  deserunt possimus autem fuga? Soluta maxime corporis quasi dolor at,
+                  vero, laboriosam vel dolorem eaque enim molestias omnis non inventore!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+                  quibusdam, dolores sit possimus natus beatae debitis ratione officia
+                  obcaecati exercitationem, dolore blanditiis ipsam cupiditate eius,
+                  repellat nisi voluptatum quam aliquid.
+                </p>
+              </Plaintext>
+          </div>
+        </div>
+      </div>
+
+ 
     </>
   );
 }
